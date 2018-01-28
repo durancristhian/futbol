eeabc1573468f5f4c505edbe8cfd49863e7da9a3
cristhian <durancristhian@gmail.com>
2018-01-27 22:28:32 -0300

    Finish redesign

--- dist/js/index.js
+++ dist/js/index.js
@@ -33,33 +33,37 @@ function init() {
                 rowsEl.insertAdjacentHTML(
                     'beforeEnd',
                     `<div class="b--black-20 bb flex relative">
-                        <div class="absolute bottom-0 left-0 o-20 right-0 top-0" style="background-color: ${
-                            playerStats.Puntos === topScore ? '#357EDD' : 'rgba(0, 0, 0, 0.2)'
+                        <div class="absolute bottom-0 left-0 right-0 top-0" style="background-color: ${
+                            playerStats.Puntos === topScore ? '#D3EBF2' : 'rgba(0, 0, 0, 0.05)'
                         }; width: ${scoreBackgroundWidth}%; z-index: -1;"></div>
-                        <div class="b--black-20 br cell-1 flex items-center justify-end pa1 pa2-ns">
+                        <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${index + 1}</span>
                         </div>
-                        <div class="b--black-20 br cell-2 flex fw5 items-center pa1 pa2-ns">
-                            <img src="${
-                                playerStats.Twitter
-                                    ? `https://avatars.io/twitter/${playerStats.Twitter}`
-                                    : 'https://placehold.it/32x32/AAAAAA?text=&nbsp;'
-                            }" alt="${playerStats['Jugador/a']}" class="h2 mr1 mr3-ns w2"/>
-                            <span class="truncate">${playerStats['Jugador/a']}</span>
+                        <div class="b--black-20 br flex flex-auto items-center pa1 pa2-ns">
+                            <div class="h2 mr1 mr3-ns overlay-blue w2">
+                                <img src="${
+                                    playerStats.Twitter
+                                        ? `https://avatars.io/twitter/${playerStats.Twitter}`
+                                        : 'https://placehold.it/32x32/AAAAAA?text=&nbsp;'
+                                }" alt="${playerStats['Jugador/a']}" class="grayscale"/>
+                            </div>
+                            <span class="${playerStats.Puntos === topScore ? 'b' : ''} truncate">${
+                        playerStats['Jugador/a']
+                    }</span>
                         </div>
-                        <div class="b--black-20 br cell-3 flex items-center pa1 pa2-ns">
+                        <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${playerStats.Jugados}</span>
                         </div>
-                        <div class="b--black-20 br cell-4 flex items-center pa1 pa2-ns">
+                        <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${playerStats.Ganados}</span>
                         </div>
-                        <div class="b--black-20 br cell-5 flex items-center pa1 pa2-ns">
+                        <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${playerStats.Empatados}</span>
                         </div>
-                        <div class="b--black-20 br cell-6 flex items-center pa1 pa2-ns">
+                        <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${playerStats.Perdidos}</span>
                         </div>
-                        <div class="b--black-20 br cell-7 flex fw5 items-center pa1 pa2-ns">
+                        <div class="b b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${playerStats.Puntos}</span>
                         </div>
                     </div>`
