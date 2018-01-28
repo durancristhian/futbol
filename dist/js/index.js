ce47fe4befb1c6b2906424142bf491c64289b2ef
cristhian <durancristhian@gmail.com>
2018-01-28 01:10:20 -0300

    Version 1.5.0

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,8 +1,7 @@
-let errorEl = document.querySelector('#error');
-let headersEl = document.querySelector('#headers');
-let loadingEl = document.querySelector('#loading');
-let rowsContainerEl = document.querySelector('#rows-container');
-let rowsEl = document.querySelector('#rows');
+const errorEl = document.querySelector('#error');
+const loadingEl = document.querySelector('#loading');
+const rowsContainerEl = document.querySelector('#rows-container');
+const rowsEl = document.querySelector('#rows');
 
 window.addEventListener('load', init);
 
@@ -32,20 +31,21 @@ function init() {
 
                 rowsEl.insertAdjacentHTML(
                     'beforeEnd',
-                    `<div class="b--black-20 bb flex relative">
-                        <div class="absolute bottom-0 left-0 right-0 top-0" style="background-color: ${
-                            playerStats.Puntos === topScore ? '#D3EBF2' : 'rgba(0, 0, 0, 0.05)'
-                        }; width: ${scoreBackgroundWidth}%; z-index: -1;"></div>
+                    `<div class="b--black-20 bb bg-white flex relative z-0">
+                        <div class="${
+                            playerStats.Puntos === topScore ? 'bg-washed-yellow' : 'bg-black-05'
+                        } absolute bottom-0 left-0 right-0 top-0" style="width: ${scoreBackgroundWidth}%; z-index: -1;"></div>
                         <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
                             <span>${index + 1}</span>
                         </div>
-                        <div class="b--black-20 br flex flex-auto items-center pa1 pa2-ns">
-                            <div class="h2 mr1 mr3-ns overlay-blue w2">
-                                <img src="${
+                        <div class="b--black-20 br cell-name flex items-center pa1 pa2-ns">
+                            <div class="h2 mr1 mr3-ns overlay w2">
+                                <div class="bg-center cover grayscale h2 w2" style="background-image: url('${
                                     playerStats.Twitter
                                         ? `https://avatars.io/twitter/${playerStats.Twitter}`
-                                        : 'https://placehold.it/32x32/AAAAAA?text=&nbsp;'
-                                }" alt="${playerStats['Jugador/a']}" class="grayscale"/>
+                                        : 'https://placehold.it/32x32/FFFFFF?text=&nbsp;'
+                                }')">
+                                </div>
                             </div>
                             <span class="${playerStats.Puntos === topScore ? 'b' : ''} truncate">${
                         playerStats['Jugador/a']
@@ -71,7 +71,6 @@ function init() {
             });
 
             loadingEl.classList.add('dn');
-            headersEl.classList.remove('dn');
             rowsContainerEl.classList.remove('dn');
         })
         .catch(function(error) {
