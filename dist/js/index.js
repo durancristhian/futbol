d1e598aca79263b1c5b692da0029c7c1ca02c105
cristhian <durancristhian@gmail.com>
2018-01-28 02:44:39 -0300

    Version 1.5.2

--- dist/js/index.js
+++ dist/js/index.js
@@ -25,29 +25,30 @@ function init() {
             );
 
             playersStats.forEach(function(playerStats, index) {
+                const isTop = playerStats.Puntos === topScore;
+                const playerIndex = index + 1;
+                const playerPicture = playerStats.Twitter
+                    ? `https://avatars.io/twitter/${playerStats.Twitter}`
+                    : 'https://placehold.it/32x32/bbbbbb?text=&nbsp;';
+                const scoreBackgroundColor = isTop ? 'bg-secondary' : 'bg-black-05';
                 const scoreBackgroundWidth = Number(
                     (playerStats.Puntos / (maximumOfGamesPlayed * 3) * 100).toFixed(2)
                 );
 
                 rowsEl.insertAdjacentHTML(
                     'beforeEnd',
-                    `<div class="b--black-20 bb bg-white flex relative z-0">
-                        <div class="${
-                            playerStats.Puntos === topScore ? 'bg-washed-yellow' : 'bg-black-05'
-                        } absolute bottom-0 left-0 right-0 top-0" style="width: ${scoreBackgroundWidth}%; z-index: -1;"></div>
+                    `<div class="b--black-20 bb bg-white-60 flex relative z-0">
+                        <div class="${scoreBackgroundColor} absolute bottom-0 left-0 right-0 top-0" style="width: ${scoreBackgroundWidth}%; z-index: -1;">
+                        </div>
                         <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                            <span>${index + 1}</span>
+                            <span>${playerIndex}</span>
                         </div>
                         <div class="b--black-20 br cell-name flex items-center pa1 pa2-ns">
                             <div class="h2 mr1 mr3-ns w2">
-                                <div class="bg-center cover h2 w2" style="background-image: url('${
-                                    playerStats.Twitter
-                                        ? `https://avatars.io/twitter/${playerStats.Twitter}`
-                                        : 'https://placehold.it/32x32/b9d7ea?text=&nbsp;'
-                                }')">
+                                <div class="bg-center cover h2 w2" style="background-image: url('${playerPicture}')">
                                 </div>
                             </div>
-                            <span class="${playerStats.Puntos === topScore ? 'b' : ''} truncate">${
+                            <span class="${isTop ? 'b' : ''} truncate">${
                         playerStats['Jugador/a']
                     }</span>
                         </div>
