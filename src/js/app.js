21bd6a0a15c2a731a99105c7db6c38035584cea5
cristhian <durancristhian@gmail.com>
2018-02-11 16:44:48 -0300

    add covers + big-refactor

--- src/js/app.js
+++ src/js/app.js
@@ -1,99 +1,16 @@
 // módulo para usar una Google Spreadsheet como base de datos
 import * as gsheets from 'gsheets';
 
-import { getSharedDOMElements } from './shared-DOM-elements';
+import { assignThemeTriggersListeners } from './theme-triggers';
 import { initThemeManager } from './theme-manager';
-
-let themeManager;
-
-// asigna a los elementos que permiten cambiar el tema una función que:
-//      - evita que esos links cambien la URL
-//      - actualiza el tema en la app
-function assignThemeTriggersListeners() {
-    const { themeTriggersEl } = getSharedDOMElements();
-
-    // recibe el evento, lo cancela y actualiza el tema
-    function themeTriggerListener(event) {
-        event.preventDefault();
-
-        themeManager.saveAndAssignTheme(event.target.dataset.theme);
-    }
-
-    // por cada uno de los links asigna el listener
-    themeTriggersEl.forEach((themeTriggerEl) => {
-        themeTriggerEl.addEventListener('click', themeTriggerListener);
-    });
-}
-
-// recibe un array con los resultados provenientes de la planilla y dibuja la grilla
-function render(playersStats) {
-    const { rowsEl } = getSharedDOMElements();
-
-    // número máximo de partidos jugados por un jugador
-    const maximumOfGamesPlayed = Math.max(
-        ...playersStats.map((playerStats) => playerStats.Jugados)
-    );
-    // puntaje más alto que puede estar compartido entre varios jugadores
-    const topScore = Math.max(...playersStats.map((playerStats) => playerStats.Puntos));
-
-    // limpiamos la grilla
-    rowsEl.innerHTML = '';
-
-    playersStats.forEach((playerStats, index) => {
-        // está primero?
-        const isTop = playerStats.Puntos === topScore;
-        // indice para mostrar
-        const playerIndex = index + 1;
-        // construimos la URL para mostrar su foto de perfil o ponemos una por defecto
-        const playerPicture = playerStats.Foto
-            ? `https://avatars.io/${playerStats.Foto}`
-            : 'https://placehold.it/32x32/bbbbbb?text=&nbsp;';
-        // usamos un color diferente si el jugador está primero
-        const scoreBackgroundColor = isTop ? 'bg-secondary' : 'bg-black-05';
-        // calculamos el ancho de la barra de progreso usada como fondo de cada fila en base
-        // a los puntos ganados por el jugador en base al total de partidos
-        const scoreBackgroundWidth = Number(
-            (playerStats.Puntos / (maximumOfGamesPlayed * 3) * 100).toFixed(2)
-        );
-
-        rowsEl.insertAdjacentHTML(
-            'beforeEnd',
-            `<div class="b--moon-gray bb br bg-white-60 flex relative z-0">
-                <div class="${scoreBackgroundColor} absolute bottom-0 left-0 right-0 top-0" style="width: ${scoreBackgroundWidth}%; z-index: -1;">
-                </div>
-                <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerIndex}</span>
-                </div>
-                <div class="b--black-20 br cell-name flex items-center pa1 pa2-ns">
-                    <div class="h2 mr1 mr3-ns w2">
-                        <div class="bg-center cover h2 w2" style="background-image: url('${playerPicture}')">
-                        </div>
-                    </div>
-                    <span class="${isTop ? 'b' : ''} truncate">${playerStats['Jugador/a']}</span>
-                </div>
-                <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerStats.Jugados}</span>
-                </div>
-                <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerStats.Ganados}</span>
-                </div>
-                <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerStats.Empatados}</span>
-                </div>
-                <div class="b--black-20 br cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerStats.Perdidos}</span>
-                </div>
-                <div class="b b--black-20 cell-number flex items-center justify-end pa1 pa2-ns">
-                    <span>${playerStats.Puntos}</span>
-                </div>
-            </div>`
-        );
-    });
-}
+import { renderCovers } from './covers';
+import { renderPositions } from './positions';
 
 export function init() {
-    const { errorEl, loadingEl, rowsContainerEl } = getSharedDOMElements();
-    themeManager = initThemeManager();
+    const errorEl = document.querySelector('#error');
+    const loadingEl = document.querySelector('#loading');
+    const mainContainerEl = document.querySelector('#main-container');
+    const themeManager = initThemeManager();
 
     // obtenemos el tema de localStorage
     const theme = themeManager.getFromLocalStorage();
@@ -107,24 +24,34 @@ export function init() {
         themeManager.saveAndAssignTheme('theme-yellow');
     }
 
-    // obtenemos los datos de la planilla
-    gsheets
-        .getWorksheetById(process.env.SPREADSHEET_ID, process.env.WORKSHEET_ID)
-        .then((worksheet) => worksheet.data)
-        .then((playersStats) => {
-            // hack para dibujar la grilla en el próximo tick
+    Promise.all([
+        // obtenemos las posiciones
+        gsheets
+            .getWorksheetById(process.env.SPREADSHEET_ID, process.env.POSITIONS_WORKSHEET_ID)
+            .then((worksheet) => worksheet.data),
+        // obtenemos las portadas
+        gsheets
+            .getWorksheetById(process.env.SPREADSHEET_ID, process.env.COVERS_WORKSHEET_ID)
+            .then((worksheet) => worksheet.data),
+    ])
+        .then(([positions, covers]) => {
             setTimeout(() => {
-                render(playersStats);
-
+                // renderizamos las posiciones
+                renderPositions(positions);
+                // renderizamos las pportadas
+                renderCovers(covers);
+                // ocultamos el spinner
                 loadingEl.classList.add('dn');
-                // mostramos la grilla después de haberla generado
-                rowsContainerEl.classList.remove('dn');
+                // mostramos la interfaz después de haberla generado
+                mainContainerEl.classList.remove('dn');
 
-                assignThemeTriggersListeners();
+                assignThemeTriggersListeners(themeManager);
             }, 0);
         })
         .catch((error) => {
+            // ocultamos la grilla
             loadingEl.classList.add('dn');
+            // mostramos el mensaje de error genérico
             errorEl.classList.remove('dn');
 
             // eslint-disable-next-line no-console
--- src/js/shared-DOM-elements.js
+++ src/js/shared-DOM-elements.js
@@ -7,11 +7,6 @@ export function getSharedDOMElements() {
 // inicializa un objeto con todos aquellos nodos HTML que se usan desde varios lados de la app
 export function querySharedElements() {
     sharedElements = {
-        errorEl: document.querySelector('#error'),
-        html: document.querySelector('html'),
-        loadingEl: document.querySelector('#loading'),
-        rowsContainerEl: document.querySelector('#rows-container'),
-        rowsEl: document.querySelector('#rows'),
         themeTriggersEl: document.querySelectorAll('[data-theme]'),
     };
 }
