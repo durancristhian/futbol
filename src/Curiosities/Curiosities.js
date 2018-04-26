472a834b7af2dc037996753e8494c1bfefce835f
cristhian <durancristhian@gmail.com>
2018-04-26 09:04:08 -0300

    change space between curiosities

--- src/Curiosities/Curiosities.js
+++ src/Curiosities/Curiosities.js
@@ -13,7 +13,7 @@ class Curiosities extends PureComponent {
     return (
       <div className="mv4 mv5-ns">
         <h2 className="f4 f3-ns mb4 mb5-ns mt0 normal tc">Curiosidades</h2>
-        <div className="flex flex-wrap justify-between nl3-l nr3-l">
+        <div className="flex flex-wrap nl3-l nr3-l">
           {curiosities.map((curiosity, index) => (
             <Curiosity key={curiosity.Descripcion} curiosity={curiosity} index={index} />
           ))}
