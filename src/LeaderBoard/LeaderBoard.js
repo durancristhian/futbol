f6ba987eddf7826ef4865e47dbde01444eb4e256
cristhian <durancristhian@gmail.com>
2018-04-28 20:42:06 -0300

    remove useless class

--- src/LeaderBoard/LeaderBoard.js
+++ src/LeaderBoard/LeaderBoard.js
@@ -42,7 +42,7 @@ class LeaderBoard extends PureComponent {
     return (
       <div className="mv4 mv5-ns">
         <h2 className="f4 f3-ns mb4 mb5-ns mt0 normal tc">Los mejores</h2>
-        <div className="flex items-end justify-center">
+        <div className="flex justify-center">
           <ul className="b--black-20 bb flex items-end justify-center list ph3 ph4-ns mv0">
             {leaders.map((leader, index) => {
               const {
