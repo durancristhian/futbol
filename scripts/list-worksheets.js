291bf50c2a37231e965152c0eb88b46b5c3db924
cristhian <durancristhian@gmail.com>
2018-05-11 18:32:47 -0300

    fix(errors): change some error messages

--- scripts/list-worksheets.js
+++ scripts/list-worksheets.js
@@ -8,12 +8,10 @@ Promise.resolve(sheetID)
   .then(
     (sheetID) =>
       typeof sheetID === 'undefined'
-        ? Promise.reject('You have to specify a sheet as the env SPREADSHEET_ID')
+        ? Promise.reject('You have to specify a process.env.SPREADSHEET_ID')
         : Promise.resolve(sheetID)
   )
   .then(getSpreadsheet)
   .then((response) => response.worksheets)
-  // eslint-disable-next-line
   .then((worksheets) => worksheets.forEach((w) => console.log(`${w.title}: ${w.id}`)))
-  // eslint-disable-next-line
-  .catch((err) => console.error(`There is a problem with the spreadsheet (${sheetID}): ${err}`));
+  .catch(console.error);
